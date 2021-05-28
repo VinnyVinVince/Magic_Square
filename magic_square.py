38f2cb69f1193b083bf597e6d45e511b4387863b
Vinny D <thevinnycast@gmail.com>
2021-05-28 09:00:49 -0700

    Board diagonal validation prepared for future recursion

--- magic_square.py
+++ magic_square.py
@@ -12,12 +12,41 @@ class MagicSquare:
     def __remove(self, row, col):
         self.__board[row][col] = 0
 
+    # Sequential search; consider alternatives for 2D array?
     def __contains(self, num):
         for arr in self.__board:
             for current in arr:
                 if current == num:
                     return True
         return False
+
+    def __check_neg_diag(self, row, col, num):
+        if row != col:
+            return True
+        sum = 0
+        for i in range(self.__size):
+            sum += self.__board[i][i]
+        if (row == (self.__size - 1)) and ((num + sum) != self.__magic_constant):
+            return False
+        return sum <= self.__magic_constant
+
+    def __check_pos_diag(self, row, col, num):
+        if (row + col) != (self.__size - 1):
+            return True
+        sum = 0
+        r = self.__size - 1
+        for c in range(self.__size):
+            sum += self.__board[r][c]
+            r -= 1
+        if (col == (self.__size - 1)) and ((num + sum) != self.__magic_constant):
+            return False
+        return sum <= self.__magic_constant
+
+    def __check_valid(self, row, col, num):
+        if (self.__board[row][col] != 0) or self.__contains(num):
+            return False
+        if not (self.__check_neg_diag(row, col, num) and self.__check_pos_diag(row, col, num)):
+            return False
 
 
 def main():
