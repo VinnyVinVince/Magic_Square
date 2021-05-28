8189edff5eb6a3e247273d32a55b5b0f739e3034
Vinny D <thevinnycast@gmail.com>
2021-05-28 08:39:43 -0700

    Basic mutators for board array

--- magic_square.py
+++ magic_square.py
@@ -1,15 +1,23 @@
 class MagicSquare:
     def __init__(self, size):
         if size < 3:
-            raise Exception("Size is too small.")
+            raise Exception("Size must be 3 or greater.")
+        self.__size = size
         self.__magic_constant = int(((size ** 3) + size) / 2)
-        self.__board = [[-1 for i in range(size)] for j in range(size)]
+        self.__board = [[0 for i in range(size)] for j in range(size)]
 
-    def get_size(self):
-        return len(self.__board)
+    def __place(self, row, col, num):
+        self.__board[row][col] = num
 
-    def remove(self, row, col):
+    def __remove(self, row, col):
         self.__board[row][col] = 0
+
+    def __contains(self, num):
+        for arr in self.__board:
+            for current in arr:
+                if current == num:
+                    return True
+        return False
 
 
 def main():
