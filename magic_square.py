fb76e83cee00c3cbc7e48380bad32b622ad41d92
Vinny D <thevinnycast@gmail.com>
2021-05-27 20:42:39 -0700

    Class variables set up

--- magic_square.py
+++ magic_square.py
@@ -1,10 +1,19 @@
 class MagicSquare:
-    def __int__(self):
-        pass
+    def __init__(self, size):
+        if size < 3:
+            raise Exception("Size is too small.")
+        self.__magic_constant = int(((size ** 3) + size) / 2)
+        self.__board = [[-1 for i in range(size)] for j in range(size)]
+
+    def get_size(self):
+        return len(self.__board)
+
+    def remove(self, row, col):
+        self.__board[row][col] = 0
 
 
 def main():
-    magic_square = MagicSquare()
+    magic_square = MagicSquare(3)
 
 
 if __name__ == "__main__":
